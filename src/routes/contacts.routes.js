820c87416ca2361d0bbd45ccf327447d36b0e2f8
Sofia Besedina <besedina.sofiia@gmail.com>
2025-06-10 20:29:06 +0300

    Update contacts.routes.js

--- src/routes/contacts.routes.js
+++ src/routes/contacts.routes.js
@@ -1,17 +1,12 @@
 import express from 'express';
-import { handleGetContacts, handleGetContactById } from '../controllers/contacts.controller.js';
+import {
+  handleGetContacts,
+  handleGetContactById,
+} from '../controllers/contacts.controller.js';
 
 const router = express.Router();
-router.get('/contacts', handleGetContacts);
-router.get('/contacts/:contactId', handleGetContactById);
+
+router.get('/', handleGetContacts);
+router.get('/:contactId', handleGetContactById);
 
 export default router;
-
-// import express from 'express';
-// import { getContactsController } from '../controllers/contacts.controller';
-
-// const router = express.Router();
-
-// router.get('/', getContactsController);
-
-// export default router;
