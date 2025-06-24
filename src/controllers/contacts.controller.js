a0f926c4b9596734d4fb337dd211388b0e6029e1
Sofia Besedina <besedina.sofiia@gmail.com>
2025-06-24 21:27:24 +0300

    test 5

--- src/controllers/contacts.controller.js
+++ src/controllers/contacts.controller.js
@@ -1,6 +1,51 @@
 
+// import * as contactsService from "../services/contacts.service.js";
+// import createError from "http-errors";
+// export const getContactById = async (req, res) => {
+//   const { contactId } = req.params;
+//   const contact = await contactsService.getContactById(contactId);
+//   if (!contact) throw createError(404, "Contact not found");
+//   res.status(200).json({ status: 200, data: contact });
+// };
+
+// export const deleteContact = async (req, res) => {
+//   const { contactId } = req.params;
+//   const isDeleted = await contactsService.deleteContact(contactId);
+//   if (!isDeleted) throw createError(404, "Contact not found");
+//   res.status(204).send();
+// };
+
+
+// export const getAllContacts = async (req, res) => {
+//   const {
+//     page = 1,
+//     perPage = 10,
+//     sortBy = 'name',
+//     sortOrder = 'asc',
+//     type,
+//     isFavourite
+//   } = req.query;
+
+
+//   const result = await contactsService.listContacts({
+//     page: Number(page),
+//     perPage: Number(perPage),
+//     sortBy,
+//     sortOrder,
+//     type,
+//     isFavourite
+//   });
+
+//   res.status(200).json({
+//     status: 200,
+//     message: "Successfully found contacts!",
+//     data: result
+//   });
+// };
+
 import * as contactsService from "../services/contacts.service.js";
 import createError from "http-errors";
+
 export const getContactById = async (req, res) => {
   const { contactId } = req.params;
   const contact = await contactsService.getContactById(contactId);
@@ -15,7 +60,6 @@ export const deleteContact = async (req, res) => {
   res.status(204).send();
 };
 
-
 export const getAllContacts = async (req, res) => {
   const {
     page = 1,
@@ -25,7 +69,6 @@ export const getAllContacts = async (req, res) => {
     type,
     isFavourite
   } = req.query;
-
 
   const result = await contactsService.listContacts({
     page: Number(page),
@@ -41,4 +84,16 @@ export const getAllContacts = async (req, res) => {
     message: "Successfully found contacts!",
     data: result
   });
+};
+
+export const createContact = async (req, res) => {
+  const newContact = await contactsService.createContact(req.body);
+  res.status(201).json({ status: 201, data: newContact });
+};
+
+export const updateContact = async (req, res) => {
+  const { contactId } = req.params;
+  const updatedContact = await contactsService.updateContact(contactId, req.body);
+  if (!updatedContact) throw createError(404, "Contact not found");
+  res.status(200).json({ status: 200, data: updatedContact });
 };
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,3 +1,72 @@
+// import express from "express";
+// import * as contactsController from "../controllers/contacts.controller.js";
+// import ctrlWrapper from "../utils/ctrlWrapper.js";
+// import { validateBody } from "../middlewares/validateBody.js";
+// import { isValidId } from "../middlewares/isValidId.js";
+// import {
+//   createContactSchema,
+//   updateContactSchema,
+// } from "../schemas/contactSchemas.js";
+
+// const router = express.Router();
+
+// router.get("/", ctrlWrapper(contactsController.getAllContacts));
+
+
+// router.get("/:contactId", isValidId, ctrlWrapper(contactsController.getContactById));
+
+
+// router.post(
+//   "/",
+//   validateBody(createContactSchema),
+//   ctrlWrapper(contactsController.createContact)
+// );
+
+
+// router.patch(
+//   "/:contactId",
+//   isValidId,
+//   validateBody(updateContactSchema),
+//   ctrlWrapper(contactsController.updateContact)
+// );
+
+// router.delete("/:contactId", isValidId, ctrlWrapper(contactsController.deleteContact));
+
+// export default router;
+
+// // import express from "express";
+// // import * as contactsController from "../controllers/contacts.controller.js";
+// // import ctrlWrapper from "../utils/ctrlWrapper.js";
+// // import { validateBody } from "../middlewares/validateBody.js";
+// // import { isValidId } from "../middlewares/isValidId.js";
+// // import {
+// //   createContactSchema,
+// //   updateContactSchema
+// // } from "../schemas/contactSchemas.js";
+
+// // const router = express.Router();
+
+// // router.get("/", ctrlWrapper(contactsController.getAllContacts));
+
+// // router.get("/:contactId", isValidId, ctrlWrapper(contactsController.getContactById));
+
+// // router.post(
+// //   "/",
+// //   validateBody(createContactSchema),
+// //   ctrlWrapper(contactsController.createContact)
+// // );
+
+// // router.patch(
+// //   "/:contactId",
+// //   isValidId,
+// //   validateBody(updateContactSchema),
+// //   ctrlWrapper(contactsController.updateContact)
+// // );
+
+// // router.delete("/:contactId", isValidId, ctrlWrapper(contactsController.deleteContact));
+// // export default router;
+
+
 import express from "express";
 import * as contactsController from "../controllers/contacts.controller.js";
 import ctrlWrapper from "../utils/ctrlWrapper.js";
@@ -12,16 +81,13 @@ const router = express.Router();
 
 router.get("/", ctrlWrapper(contactsController.getAllContacts));
 
-
 router.get("/:contactId", isValidId, ctrlWrapper(contactsController.getContactById));
-
 
 router.post(
   "/",
   validateBody(createContactSchema),
   ctrlWrapper(contactsController.createContact)
 );
-
 
 router.patch(
   "/:contactId",
@@ -33,36 +99,3 @@ router.patch(
 router.delete("/:contactId", isValidId, ctrlWrapper(contactsController.deleteContact));
 
 export default router;
-
-// import express from "express";
-// import * as contactsController from "../controllers/contacts.controller.js";
-// import ctrlWrapper from "../utils/ctrlWrapper.js";
-// import { validateBody } from "../middlewares/validateBody.js";
-// import { isValidId } from "../middlewares/isValidId.js";
-// import {
-//   createContactSchema,
-//   updateContactSchema
-// } from "../schemas/contactSchemas.js";
-
-// const router = express.Router();
-
-// router.get("/", ctrlWrapper(contactsController.getAllContacts));
-
-// router.get("/:contactId", isValidId, ctrlWrapper(contactsController.getContactById));
-
-// router.post(
-//   "/",
-//   validateBody(createContactSchema),
-//   ctrlWrapper(contactsController.createContact)
-// );
-
-// router.patch(
-//   "/:contactId",
-//   isValidId,
-//   validateBody(updateContactSchema),
-//   ctrlWrapper(contactsController.updateContact)
-// );
-
-// router.delete("/:contactId", isValidId, ctrlWrapper(contactsController.deleteContact));
-
-// export default router;
