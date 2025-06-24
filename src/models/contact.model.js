523a5fcd2d8e730d8e71b45dfe159277bf8cdbae
Sofia Besedina <besedina.sofiia@gmail.com>
2025-06-24 21:13:51 +0300

    test4

--- src/models/contact.model.js
+++ src/models/contact.model.js
@@ -6,7 +6,11 @@ const contactSchema = new mongoose.Schema(
     phoneNumber: { type: String, required: true },
     email: { type: String },
     isFavourite: { type: Boolean, default: false },
-    contactType: { type: String, required: true },
+    contactType: {
+      type: String,
+      required: true,
+      enum: ["work", "home", "personal"], 
+    },
   },
   { timestamps: true }
 );
